b8d75dec9ac58224fd166cf51142509a0b1b39a1
Raissa Coelho <raissacoelho.1995@gmail.com>
2024-08-16 21:01:47 -0300

    add router and sass

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,89 +1,58 @@
 <template>
+  <div id="app">
 
-  <main>
-    <div id="app">
-    </div> 
-    <section class="header">
     <b-navbar>
-        <template #brand>
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-                <img
-                    src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-                    alt="Lightweight UI components for Vue.js based on Bulma"
-                >
-            </b-navbar-item>
-        </template>
-        <template #start>
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-              Home
-            </b-navbar-item>
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-                Documentação
-            </b-navbar-item>
-            <b-navbar-dropdown label="Info">
-                <b-navbar-item tag="router-link" :to="{ path: '/about'}">
-                    Sobre
-                </b-navbar-item>
-                <b-navbar-item href="#">
-                    Contato
-                </b-navbar-item>
-            </b-navbar-dropdown>
-        </template>
-
-        <template #end>
-            <b-navbar-item tag="div">
-                <div class="buttons">
-                    <a class="button is-primary">
-                        <strong>Sign up</strong>
-                    </a>
-                    <a class="button is-light">
-                        Log in
-                    </a>
-                </div>
-            </b-navbar-item>
-        </template>
+      <template #brand>
+        <b-navbar-item tag="router-link" :to="{ name: 'Home' }">
+          <img
+            src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+            alt="Buefy Logo"
+          >
+        </b-navbar-item>
+      </template>
+      <template #start>
+        <b-navbar-item tag="router-link" :to="{ name: 'Home' }">
+          Home
+        </b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ name: 'about' }">
+          Sobre
+        </b-navbar-item>
+      </template>
+      <template #end>
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">
+              Log in
+            </a>
+          </div>
+        </b-navbar-item>
+      </template>
     </b-navbar>
     <router-view :key="$route.fullPath"></router-view>
-    </section>
 
+  
     <section class="footer">
       <footer class="footer">
         <div class="content has-text-centered">
-         <p>
-           <strong>Horário Fácil</strong> por Nomes.
+          <p>
+            <strong>Horário Fácil</strong> por Nomes.
           </p>
-          </div>
+        </div>
       </footer>
     </section>
-    
-  </main>
+ 
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+export default {
+  name: 'App',
+};
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<style lang="scss" scooped>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
